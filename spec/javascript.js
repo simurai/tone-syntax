791d541010d90d9d1f6791f9d0e965a221492669
simurai <simulus@gmail.com>
2016-12-12 12:51:57 +0900

    Add specs

---
+++ spec/javascript.js
@@ -0,0 +1,32 @@
+
+var Sales = "Toyota";
+
+function CarTypes(name) {
+  if (name == "Honda") {
+    return name;
+  } else {
+    return "Sorry, we don't sell " + name + ".";
+  }
+}
+
+var car = { myCar: "Saturn", getCar: CarTypes("Honda"), special: Sales };
+
+console.log(car.special);
+
+
+// 2. Tests ----------------------
+
+
+return {
+  restrict: 'E',
+  transclude: true,
+  scope: {
+    options: '='
+  },
+  templateUrl: 'js/...',
+  controllerAs: 'vm',
+  bindToController: true,
+  controller: function (NgTableParams, TABLE_OPTIONS_FIELD) {
+    var vm = this;
+  }
+};
