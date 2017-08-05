fd3d97e8fbaaee1c1c1ce75d9ccefe5cb29bf912
Cooper Maruyama <me@cooperm.com>
2017-08-05 00:03:56 -0700

    feat(js): ES6 styles

--- spec/javascript.js
+++ spec/javascript.js
@@ -13,6 +13,32 @@ var car = { myCar: "Saturn", getCar: CarTypes("Honda"), special: Sales };
 
 console.log(car.special);
 
+// ES6 ---------------------------
+
+const templateString = `${car.myCar} is a ${Sales} yay`;
+
+const obj = {
+  [myKey]: 'val',
+  method(x, y) {
+    return x ** y;
+  },
+  ...(foo ? obj1 : obj2)
+}
+
+class MyClass extends BaseClass {
+  constructor(...args) {
+    super(...args);
+
+    this.foo = args.foo;
+  }
+
+  fooMethod(bar = 'defaultValue', { items, baz: qux, ...rest, }) {
+    this.arr = [...items];
+
+    return this.arr.map(x => x * 2);
+  }
+}
+
 
 // 2. Tests ----------------------
 
