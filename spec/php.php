f45f3996b09176d2a4c6abf010c7989919b2db10
Maarten de Boer <maarten@cloudstek.nl>
2018-03-28 14:47:04 +0200

    Update PHP styles

--- spec/php.php
+++ spec/php.php
@@ -21,3 +21,42 @@
     };
   }
 ?>
+
+<?php
+  namespace Tone\Syntax;
+
+  use Tone\Syntax\Traits\MyTrait;
+
+  class Test
+  {
+    use MyTrait;
+
+    /**
+     * Docblock
+     * @var string|null
+     */
+    private $property;
+
+    /**
+     * Get property
+     *
+     * @return string|null My fancy description of this property
+     */
+    public function getProperty() : ?string
+    {
+      return $this->property;
+    }
+
+    /**
+     * Set property
+     *
+     * @param string|null $property
+     */
+    public function setProperty(?string $property) : void
+    {
+      $this->property = $property;
+
+      $this->logInfo('Property has been set.');
+    }
+  }
+?>
